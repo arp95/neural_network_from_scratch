d593d6387be01929a4a0fa54374028539a16c341
Arpit Aggarwal <arpitdec5@gmail.com>
2020-03-02 22:13:46 -0500

    Updated metrics.py file

--- Code/metrics.py
+++ Code/metrics.py
@@ -4,3 +4,7 @@ import numpy as np
 # function for calculating mean square error
 def mean_square_loss(y_original, y_pred):
     return np.mean((y_pred - y_original) ** 2)
+
+# function to calculate accuracy
+def accuracy(y_original, y_pred):
+    return np.mean(y_original == y_pred)
